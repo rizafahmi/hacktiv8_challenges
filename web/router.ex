5d64d30f2add37800d73c11be53d84a29f2e2d04
Riza Fahmi <rizafahmi@gmail.com>
2016-07-14 18:04:39 +0700

    Finish iteration #1

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Hacktiv8Challenges.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/read/:id", PageController, :show
     resources "/challenges", ChallengeController
   end
 
